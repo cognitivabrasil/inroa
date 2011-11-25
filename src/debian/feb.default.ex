92d21b97e675d47d496eaf39fec3aa98ad56b35b
Paulo Schreiner <paulo.schreiner@gmail.com>
2011-11-25 16:13:09 +0000

    Mais arquivos.

---
+++ src/debian/feb.default.ex
@@ -0,0 +1,10 @@
+# Defaults for feb initscript
+# sourced by /etc/init.d/feb
+# installed at /etc/default/feb by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
