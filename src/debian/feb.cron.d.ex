b9d155b12c3daad26f904e46aaddcdd1bcb85012
Paulo Schreiner <paulo.schreiner@gmail.com>
2011-11-28 18:38:46 +0000

    Adicionei arquivos padrao GNU.

--- src/debian/feb.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the feb package
-#
-0 4	* * *	root	[ -x /usr/bin/feb_maintenance ] && /usr/bin/feb_maintenance
--- src/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(feb):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="feb" command="/usr/bin/feb"
