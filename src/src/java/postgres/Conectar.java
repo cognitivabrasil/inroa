757c027b05b4d741784d27192e1a104d9e5e18bd
Paulo Schreiner <paulo.schreiner@gmail.com>
2012-01-17 16:59:46 +0000

    Reversed

--- src/src/java/postgres/Conectar.java
+++ src/src/java/postgres/Conectar.java
@@ -1,13 +1,12 @@
 package postgres;
 
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import javax.sql.DataSource;
-import org.springframework.context.ApplicationContext;
-import spring.ApplicationContextProvider;
 
 /**
  * Disponibiliza método para conectar na base de dados Postgres.
@@ -16,13 +15,12 @@ import spring.ApplicationContextProvider;
 public class Conectar {
 
     postgres.SingletonConfig conf;
-    ApplicationContext ctx;
 
     //Configuracao conf;
 
     // para criar uma conexão o JSP que chamar essa função TEM de chamar antes o SigletonConfig.initConfig(c);
     public Conectar() {
-    ctx = ApplicationContextProvider.getApplicationContext() ;
+
     conf = SingletonConfig.getConfig();
     //conf = new Configuracao();
     }
@@ -35,15 +33,33 @@ public class Conectar {
     public Connection conectaBD() {
 
         Connection con = null;
-        
-        DataSource dataSource = (DataSource)ctx.getBean("dataSource");
+
+
+        String baseDeDados = conf.getBase();
+        String usuario = conf.getUsuario();
+        String senha = conf.getSenha();
+        String ipBase = conf.getIP();
+        //String ipBase = conf.getIp();
+        int portaBase = conf.getPorta();
 
         try {
-                System.err.println("conectaDB");
-               con = dataSource.getConnection();      
+            // Este é um dos meios para registrar um driver
+            Class.forName("org.postgresql.Driver");
+
+
+
+            // Registrado o driver, vamos estabelecer uma conexão
+            con = DriverManager.getConnection("jdbc:postgresql://" + ipBase + ":" + portaBase + "/" + baseDeDados, usuario, senha);
+
+
+            //         return con;
+
+        } catch (ClassNotFoundException e) {
+            System.err.println("FEB ERRO: Classe não encontrada ao conectar na base de dados. Mensagem: "+e.getMessage());
+
         } catch (SQLException e) {
             SimpleDateFormat dataFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-            System.err.println("FEB ERRO: Mensagem: " + e.getMessage());
+            System.err.println("FEB ERRO: "+dataFormat.format(new Date())+" SQL Exception... Erro ao efetuar a conexão na base de dados no ip: " + ipBase + " na porta: "+portaBase+". Mensagem: " + e.getMessage());
 
 //       esse codigo serve para imprimir o ClassPath do postgres
 //            //Get the System Classloader
@@ -55,6 +71,21 @@ public class Conectar {
 //                System.err.println(urls[i].getFile());
 //            }
 
+        } catch (NullPointerException n) {
+            SimpleDateFormat dataFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+            System.err.println("FEB ERRO: " + dataFormat.format(new Date()) + " NullPointer ao efetuar conexao na base de dados. Classe Conectar: " + n);
+            if (ipBase == null) {
+                System.out.println("FEB ERRO: Variavel ipBase é null");
+            }
+            if (baseDeDados == null) {
+                System.out.println("FEB ERRO: Variavel baseDeDados é null");
+            }
+            if (usuario == null) {
+                System.out.println("FEB ERRO: Variavel usuario é null");
+            }
+            if (senha == null) {
+                System.out.println("FEB ERRO: Variavel senha é null");
+            }
         }
 
         return con;
