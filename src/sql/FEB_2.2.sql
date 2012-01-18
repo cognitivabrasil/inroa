dc8ea0ef92c332134e854421af8dd221671a8f51
Marcos Nunes <marcosn@gmail.com>
2012-01-18 18:47:46 +0000

    nao tinha visto o arquivo current.sql ai criei um novo com a modificacao da base. Agora coloquei a modificacao no current e apaguei o novo.

--- src/sql/FEB_2.2.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE consultas(
-   consulta character varying NOT NULL,
-   ids text,
-   CONSTRAINT consultas_pkey PRIMARY KEY (consulta)
-) WITH (OIDS = FALSE);
-ALTER TABLE consultas OWNER TO feb;
--- src/sql/current.sql
+++ src/sql/current.sql
@@ -3,3 +3,10 @@ ALTER TABLE info_repositorios ADD COLUMN data_xml timestamp without time zone NO
 ALTER TABLE dados_subfederacoes
    ADD COLUMN data_xml timestamp without time zone NOT NULL DEFAULT '0001-01-01 00:00:00';
 
+CREATE TABLE consultas(
+   consulta character varying NOT NULL,
+   ids text,
+   CONSTRAINT consultas_pkey PRIMARY KEY (consulta)
+) WITH (OIDS = FALSE);
+
+ALTER TABLE consultas OWNER TO feb;
