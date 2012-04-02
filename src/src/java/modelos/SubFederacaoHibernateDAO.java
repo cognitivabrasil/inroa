dbfe43746ae9251c6c9a124d81e88b5cfc198e88
Paulo Schreiner <paulo.schreiner@gmail.com>
2012-04-02 16:09:53 +0000

    SubFederacaoDao implementado.
    * Impleentei get(String nome)
    * Implementei save().
    * Testes
    
    Ref: FEB-7

--- src/src/java/modelos/SubFederacaoHibernateDAO.java
+++ src/src/java/modelos/SubFederacaoHibernateDAO.java
@@ -8,6 +8,7 @@ import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate3.HibernateTemplate;
 
 /**
  *
@@ -15,7 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
  */
 //@Transactional
 public class SubFederacaoHibernateDAO implements SubFederacaoDAO {
-    
+
     @Autowired
     SessionFactory sessionFactory;
 
@@ -24,7 +25,7 @@ public class SubFederacaoHibernateDAO implements SubFederacaoDAO {
     }
 
     public SubFederacao get(int id) {
-        return (SubFederacao)this.sessionFactory.getCurrentSession().get(SubFederacao.class, id);
+        return (SubFederacao) this.sessionFactory.getCurrentSession().get(SubFederacao.class, id);
     }
 
     public List<SubFederacao> getAll() {
@@ -34,10 +35,11 @@ public class SubFederacaoHibernateDAO implements SubFederacaoDAO {
 
     public SubFederacao get(String nome) {
         Session s = this.sessionFactory.getCurrentSession();
-        return (SubFederacao)s.createQuery("from SubFederacao WHERE nome = :nome").setString("nome", nome).uniqueResult();
+        return (SubFederacao) s.createQuery("from SubFederacao WHERE nome = :nome").setString("nome", nome).uniqueResult();
     }
 
     public void save(SubFederacao s) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        HibernateTemplate t = new HibernateTemplate(this.sessionFactory);
+        t.saveOrUpdate(s);
     }
 }
--- src/src/test/java/modelos/SubfederacaoHibernateDaoIT.java
+++ src/src/test/java/modelos/SubfederacaoHibernateDaoIT.java
@@ -8,11 +8,14 @@ import java.io.File;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import javax.sql.DataSource;
+import org.dbunit.Assertion;
 import org.dbunit.database.DatabaseConnection;
 import org.dbunit.database.IDatabaseConnection;
 import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.SortedTable;
 import org.dbunit.dataset.xml.FlatXmlDataSet;
 import org.dbunit.operation.DatabaseOperation;
 import org.junit.*;
@@ -28,9 +31,8 @@ import org.springframework.test.context.support.DependencyInjectionTestExecution
 import org.springframework.transaction.annotation.Transactional;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
-
-
-
+import org.springframework.test.context.transaction.AfterTransaction;
+import org.springframework.test.context.transaction.TransactionConfiguration;
 
 /**
  *
@@ -40,6 +42,7 @@ import static org.hamcrest.Matchers.*;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:testApplicationContext.xml")
 @TestExecutionListeners({DependencyInjectionTestExecutionListener.class})
+@TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = false)
 public class SubfederacaoHibernateDaoIT extends AbstractTransactionalJUnit4SpringContextTests {
 
     @Autowired
@@ -47,6 +50,7 @@ public class SubfederacaoHibernateDaoIT extends AbstractTransactionalJUnit4Sprin
     @Autowired
     DataSource dataSource;
     static IDatabaseConnection connection;
+    boolean updated = false;
 
     public SubfederacaoHibernateDaoIT() {
     }
@@ -71,7 +75,7 @@ public class SubfederacaoHibernateDaoIT extends AbstractTransactionalJUnit4Sprin
     public void after() throws Exception {
         System.out.println("After");
         //Limpa a base de dados
-       // DatabaseOperation.DELETE_ALL.execute(getConnection(), getBeforeDataSet());
+        // DatabaseOperation.DELETE_ALL.execute(getConnection(), getBeforeDataSet());
     }
 
     private IDatabaseConnection getConnection() throws Exception {
@@ -88,16 +92,24 @@ public class SubfederacaoHibernateDaoIT extends AbstractTransactionalJUnit4Sprin
 
     }
 
+    private IDataSet getAfterDataSet() throws Exception {
+        System.out.println("get DataSet");
+        // Pega o arquivo de para inserir
+        File file = new File("src/test/resources/documentosDataAfter.xml");
+        return new FlatXmlDataSet(file);
+    }
+
     private IDataSet getBeforeDataSet() throws Exception {
         System.out.println("get DataSet");
         // Pega o arquivo de para inserir
         File file = new File("src/test/resources/documentosDataBefore.xml");
         return new FlatXmlDataSet(file);
     }
-    /**
-     * Test of delete method, of class RepositoryHibernateDAO.
 
     /**
+     * Test of delete method, of class RepositoryHibernateDAO.
+     *
+     * /**
      * Test of get method, of class RepositoryHibernateDAO.
      */
     @Test
@@ -105,11 +117,11 @@ public class SubfederacaoHibernateDaoIT extends AbstractTransactionalJUnit4Sprin
         System.out.println("get");
         int id = 1;
         SubFederacao ufrgs = instance.get(id);
-        
+
         assertEquals("UFRGS", ufrgs.getNome());
     }
-    
-   /**
+
+    /**
      * Test of getAll method, of class RepositoryHibernateDAO.
      */
     @Test
@@ -120,67 +132,96 @@ public class SubfederacaoHibernateDaoIT extends AbstractTransactionalJUnit4Sprin
         // TODO review the generated test code and remove the default call to fail.
         //fail("The test case is a prototype.");
     }
-    
+
     @Test
     @Transactional
     public void testRepository() {
         System.out.println("repository");;
-        
+
         List<SubFederacao> l = instance.getAll();
         SubFederacao ufrgs = l.get(0);
-        
+
         List<RepositorioSubFed> r = new ArrayList<RepositorioSubFed>(ufrgs.getRepositorios());
-        
+
         RepositorioSubFed t = r.get(0);
-        
+
         assertEquals("RepUfrgs1", t.getNome());
         assertEquals("Get from getDocumentos", 2, t.getDocumentos().size());
-        assertEquals("Get from size()", 2 , (int)t.getSize());
- 
+        assertEquals("Get from size()", 2, (int) t.getSize());
+
 
     }
-    
+
     @Test
-    public void testDelete() {       
+    public void testDelete() {
         SubFederacao cesta = instance.get(1);
-      
+
         instance.delete(cesta);
-     
+
         List<SubFederacao> l = instance.getAll();
         assertEquals(2, l.size());
 
-        
+
     }
-    
+
     @Test
     public void testGetByName() {
         SubFederacao ufrgs = instance.get("UFRGS");
-        
-        
+
+
         assertThat(ufrgs, is(notNullValue()));
         assertEquals("UFRGS", ufrgs.getNome());
         assertEquals(1, ufrgs.getId());
     }
-    
+
     @Test
     public void testGetByNameNonExisting() {
-         SubFederacao fake = instance.get("rgterter");
-         
-         assertThat(fake, is(nullValue()));
+        SubFederacao fake = instance.get("rgterter");
+
+        assertThat(fake, is(nullValue()));
 
     }
 
-    /**
-     * Test of save method, of class RepositoryHibernateDAO.
-     */
     @Test
-    @Ignore
-    public void testSave() {
+    public void testSaveAndUpdate() throws Exception {
         System.out.println("save");
-        Repositorio r = null;
-        RepositoryHibernateDAO instance = new RepositoryHibernateDAO();
-        instance.save(r);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+
+        SubFederacao f = new SubFederacao();
+        f.setNome("Nova");
+        f.setUrl("http://nova");
+        f.setDataXML(new Date());
+
+        instance.save(f);
+
+        SubFederacao fTeste = instance.get("Nova");
+        assertThat(fTeste, is(notNullValue()));
+        assertEquals("Nova", fTeste.getNome());
+
+        assertEquals("Nr correto de Subfederacoes apos adicao", 4, instance.getAll().size());
+
+        SubFederacao f2 = instance.get(3);
+        f2.setNome("Jorjao");
+        f2.setUrl("http://jorjao");
+
+        instance.save(f2);
+
+        updated = true;
+
+    }
+
+    /*
+     * This is needed to get over AbstractTransactionalJUnit4SpringContextTests
+     * limitations TODO: find a more elegant and generic solution to integrate
+     * spring and DbUnit, maybe with annotations?
+     */
+    @AfterTransaction
+    public void testSaveAndUpdate2() throws Exception {
+        if (updated) {
+            updated = false;
+            String[] ignore = {"id", "data_xml", "data_ultima_atualizacao", "descricao"};
+            String[] sort = {"nome"};
+            Assertion.assertEqualsIgnoreCols(new SortedTable(getAfterDataSet().getTable("dados_subfederacoes"), sort), new SortedTable(getConnection().createDataSet().getTable("dados_subfederacoes"), sort), ignore);
+
+        }
     }
 }
