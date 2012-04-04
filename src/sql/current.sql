b880159f72a92be14c574f425afa3dd75cadf265
Luiz Rossi <lh.rossi@gmail.com>
2012-04-04 20:18:50 +0000

    atualização parcial do current

--- src/sql/current.sql
+++ src/sql/current.sql
@@ -1,5 +1,3 @@
-ALTER TABLE info_repositorios ADD COLUMN data_xml timestamp without time zone NOT NULL DEFAULT '0001-01-01 00:00:00';
-
 ALTER TABLE dados_subfederacoes
    ADD COLUMN data_xml timestamp without time zone NOT NULL DEFAULT '0001-01-01 00:00:00';
 
@@ -19,33 +17,18 @@ CREATE TABLE mapeamentos_xslt (id INTEGER, description VARCHAR, xslt VARCHAR);
 
 ALTER TABLE mapeamentos_xslt OWNER TO feb;
 
-CREATE VIEW repositorios_view AS select * from repositorios,info_repositorios
-WHERE id = id_repositorio;
-ALTER VIEW repositorios_view OWNER TO feb;
-
-ALTER TABLE info_repositorios DROP COLUMN nome_na_federacao;
-
-
-
-DROP VIEW repositorios_view;
-
-DROP TABLE info_repositorios;
-
 DROP TABLE repositorios CASCADE;
 
-CREATE TABLE repositorios (
-  id integer DEFAULT nextval('repositorios_id_seq'::regclass) NOT NULL,
-    nome character varying(45) NOT NULL,
-    descricao character varying(455) DEFAULT NULL::character varying,
-    data_ultima_atualizacao timestamp without time zone DEFAULT '0001-01-01 00:00:00'::timestamp without time zone,
-    periodicidade_horas integer,
-    url_or_ip character varying(200) NOT NULL,
-    padrao_metadados integer,
-    mapeamento_id integer DEFAULT 1 NOT NULL,
-    metadata_prefix character varying(45),
-    name_space character varying(45),
-    set character varying(45),
-    data_xml timestamp without time zone DEFAULT '0001-01-01 00:00:00'::timestamp without time zone NOT NULL
+ALTER TABLE repositorios (            
+    ADD COLUMN data_ultima_atualizacao timestamp without time zone DEFAULT '0001-01-01 00:00:00'::timestamp without time zone,
+    ADD COLUMN periodicidade_horas integer,
+    ADD COLUMN url_or_ip character varying(200) NOT NULL,
+    ADD COLUMN padrao_metadados integer,
+    ADD COLUMN mapeamento_id integer DEFAULT 1 NOT NULL,
+    ADD COLUMN metadata_prefix character varying(45),
+    ADD COLUMN name_space character varying(45),
+    ADD COLUMN set character varying(45),
+    ADD COLUMN data_xml timestamp without time zone DEFAULT '0001-01-01 00:00:00'::timestamp without time zone NOT NULL
 );
 
 ALTER TABLE public.repositorios OWNER TO feb;
