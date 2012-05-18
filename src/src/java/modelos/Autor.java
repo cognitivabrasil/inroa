32c44ba8ed8eb8d9ef6b2b82912503ade41be7b8
Luiz Rossi <lh.rossi@gmail.com>
2012-05-18 14:53:15 +0000

    current com chave estrangeira de autores para documetos.

---
+++ src/src/java/modelos/Autor.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelos;
+
+
+/**
+ *
+ * @author cei
+ */
+public class Autor {
+    private String nome;
+    private DocumentoReal doc;
+
+    public DocumentoReal getDoc() {
+        return doc;
+    }
+
+    public void setDoc(DocumentoReal doc) {
+        this.doc = doc;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
