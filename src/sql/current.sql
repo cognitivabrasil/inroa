d65c1cd69a886354e26cb4519229bb31837b3eb9
Paulo Schreiner <paulo.schreiner@gmail.com>
2012-05-29 15:57:50 +0000

    Adicionei mapeamento para o OBAA.

--- src/sql/current.sql
+++ src/sql/current.sql
@@ -364,3 +364,18 @@ ALTER TABLE autores ALTER COLUMN id SET NOT NULL;
 ALTER TABLE autores ALTER COLUMN id SET DEFAULT nextval('autores_id_seq'::regclass);
 
 
+-- 29-6-2012
+INSERT INTO mapeamentos VALUES (3, 'OBAA Padrão', 'Mapeamento OBAA padrão, só copia os dados',
+'<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet version="2.0"
+xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" 
+xmlns:obaa="http://ltsc.ieee.org/xsd/LOM"
+xmlns:oai_pmh="http://www.openarchives.org/OAI/2.0/"
+xpath-default-namespace="http://www.openarchives.org/OAI/2.0/">
+<xsl:template match="@*|node()">
+  <xsl:copy>
+    <xsl:apply-templates select="@*|node()"/>
+  </xsl:copy>
+</xsl:template></xsl:stylesheet>', 3);
+
