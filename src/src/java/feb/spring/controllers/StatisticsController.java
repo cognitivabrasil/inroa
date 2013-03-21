93c80de2ba5925b2f246bd203c5ca93a5ba382e4
Marcos Nunes <marcosn@gmail.com>
2013-03-21 16:50:37 +0000

    FEB-311 agora documentos deletados não voltam mais nos 10 mais acessados.

--- src/src/java/feb/spring/controllers/StatisticsController.java
+++ src/src/java/feb/spring/controllers/StatisticsController.java
@@ -15,7 +15,6 @@ import org.apache.log4j.Logger;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
