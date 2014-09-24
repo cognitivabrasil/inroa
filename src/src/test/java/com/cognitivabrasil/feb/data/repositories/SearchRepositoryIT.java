7f97d50501d7a58d3b578ee8c892f14bfdd22ea9
Marcos Nunes <marcosn@gmail.com>
2014-09-24 10:31:50 -0300

    Alterei a configuração do netbeans para rodar com java 8. Removi uns imports que não eram utilizados em SearchRepositoryIT

--- src/src/test/java/com/cognitivabrasil/feb/data/repositories/SearchRepositoryIT.java
+++ src/src/test/java/com/cognitivabrasil/feb/data/repositories/SearchRepositoryIT.java
@@ -14,10 +14,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 
 import com.cognitivabrasil.feb.AppConfig;
