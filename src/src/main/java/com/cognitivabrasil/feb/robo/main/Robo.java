faf8091780f6f2443868b890422461b5960f6778
Marcos Nunes <marcosn@gmail.com>
2014-09-25 16:38:20 -0300

    Atualizando a hora que o robô vai rodar.

--- src/src/main/java/com/cognitivabrasil/feb/robo/main/Robo.java
+++ src/src/main/java/com/cognitivabrasil/feb/robo/main/Robo.java
@@ -40,7 +40,7 @@ public class Robo {
      * repositório.
      *
      */
-    @Scheduled(cron = "0 30 16 * * *")
+    @Scheduled(cron = "0 0 2 * * *")
     @Transactional
     public void run() {
 
