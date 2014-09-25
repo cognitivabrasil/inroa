a417cc438e1bb9d9d7fb145e7db15a86eea42ff3
Paulo Schreiner <paulo.schreiner@gmail.com>
2014-09-25 16:44:45 -0300

    Rodando no NetBeans.

--- src/src/main/java/com/cognitivabrasil/feb/ServletInitializer.java
+++ src/src/main/java/com/cognitivabrasil/feb/ServletInitializer.java
@@ -2,6 +2,8 @@ package com.cognitivabrasil.feb;
 
 import javax.servlet.Filter;
 import javax.servlet.ServletRegistration.Dynamic;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
@@ -18,35 +20,10 @@ import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatche
  * @see Application
  */
 public class ServletInitializer extends
-    AbstractAnnotationConfigDispatcherServletInitializer {
+    SpringBootServletInitializer {
 
-  @Override
-  protected Class<?>[] getRootConfigClasses() {
-    return new Class[] { AppConfig.class };
-  }
-
-  @Override
-  protected Class<?>[] getServletConfigClasses() {
-    return new Class[] { WebConfig.class };
-  }
-
-  @Override
-  protected String[] getServletMappings() {
-    return new String[] { "/" };
-  }
-
-  @Override
-  protected void customizeRegistration(Dynamic registration) {
-    registration.setInitParameter("dispatchOptionsRequest", "true");
-  }
-
-  @Override
-  protected Filter[] getServletFilters() {
-    CharacterEncodingFilter charFilter = new CharacterEncodingFilter();
-    charFilter.setEncoding("UTF-8");
-    charFilter.setForceEncoding(true);
-    return new Filter[] { new HiddenHttpMethodFilter(), charFilter,
-        new HttpPutFormContentFilter() };
-  }
-  
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 }
