fd6178be48060fa8114dfe3efe8e84fe5aa18d7c
Paulo Schreiner <paulo.schreiner@gmail.com>
2014-10-14 09:01:36 -0300

    Adicionei arquivo git.properties nos testes.
    Para conseguir carregar o bean e não dar erro.

--- src/src/main/java/com/cognitivabrasil/feb/AppConfig.java
+++ src/src/main/java/com/cognitivabrasil/feb/AppConfig.java
@@ -84,13 +84,13 @@ public class AppConfig {
     }
     
     @Bean(name = "gitProperties")
-    public Properties gitProperties() throws IOException {
+    public Properties gitProperties() {
         Properties properties = new Properties();
         
         try {
             properties.load(getClass().getClassLoader().getResourceAsStream("git.properties"));
         }
-        catch(IOException e) {
+        catch(IOException | NullPointerException e) {
             log.error("Não foi possível ler o arquivo git.properties, não irá mostrar informações " +
                     "a respeito do git na página de admin. Erro foi: ", e);
         }
